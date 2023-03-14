807d39c4f8615ceed46563f315a847d8f4ae4712
Duy <duytv.dev@gmail.com>
2023-03-14 13:00:52 +0700

    14/3/2023

--- src/pages/admin/dashboard.js
+++ src/pages/admin/dashboard.js
@@ -9,6 +9,7 @@ const Dashboard = () => {
 
     useEffect(function() {
         // fetch('http://localhost:3000/books')
+        // gửi và nhận request
         // .then(function(res) {
         //     return res.json()
         // })
@@ -50,40 +51,40 @@ const Dashboard = () => {
         <h1>Dashboard</h1>
         <div class="rounded-lg border border-gray-200">
             <table class="min-w-full divide-y-2 divide-gray-200 text-sm">
-            <thead>
-                <tr>
-                <th class="w-[10%] px-4 py-2 text-left font-medium text-gray-900">
-                    STT
-                </th>
-                <th class="w-[70%] px-4 py-2 text-left font-medium text-gray-900">
-                    Tên
-                </th>
-                <th class="w-[10%] px-4 py-2 text-left font-medium text-gray-900">
-                    Ảnh
-                </th>
-                <th class="w-[10%] px-4 py-2 text-left font-medium text-gray-900">
-                    Thao tác
-                </th>
-                </tr>
-            </thead>
-            <tbody class="divide-y divide-gray-200">
-            ${data.map((book, index) => /*html*/`
-                <tr>
-                    <td class="px-4 py-2 font-medium text-gray-900">
-                        ${index + 1}
-                    </td>
+                <thead>
+                    <tr>
+                        <th class="w-[10%] px-4 py-2 text-left font-medium text-gray-900">
+                            STT
+                        </th>
+                        <th class="w-[70%] px-4 py-2 text-left font-medium text-gray-900">
+                            Tên
+                        </th>
+                        <th class="w-[10%] px-4 py-2 text-left font-medium text-gray-900">
+                            Ảnh
+                        </th>
+                        <th class="w-[10%] px-4 py-2 text-left font-medium text-gray-900">
+                            Thao tác
+                        </th>
+                    </tr>
+                </thead>
+                <tbody class="divide-y divide-gray-200">
+                    ${data.map((book, index) => /*html*/`
+                        <tr>
+                            <td class="px-4 py-2 font-medium text-gray-900">
+                                ${index + 1}
+                            </td>
                     
-                    <td><a href="/admin/books/${book.id}">${book.name}</a></td>
-                    <td class="px-4 py-2 text-gray-700">
-                        <img src="${book.images?.[0].base_url}"/>
-                    </td>
-                    <td class="px-4 py-2 text-white">
-                        <button class="delete-btn p-2 bg-red-600" data-id="${book.id}">Xoá</button>
-                    </td>
-                </tr>
-            `).join('')}
+                            <td><a href="/admin/books/${book.id}">${book.name}</a></td>
+                            <td class="px-4 py-2 text-gray-700">
+                                <img src="${book.images?.[0].base_url}"/>
+                            </td>
+                            <td class="px-4 py-2 text-white">
+                                <button class="delete-btn p-2 bg-red-600" data-id="${book.id}">Xoá</button>
+                            </td>
+                        </tr>
+                    `).join('')}
                 
-            </tbody>
+                </tbody>
             </table>
         </div>
 
